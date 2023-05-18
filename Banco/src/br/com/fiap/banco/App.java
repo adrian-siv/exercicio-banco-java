a3b87a2d00fd41d36e686441a553910ddd1f20e6
adrian-siv <adrian.ssiv@gmail.com>
2023-05-18 08:26:28 -0300

    feat: add an exception for cases where the balance is less than the value entered for withdrawal or transfer

--- Banco/src/br/com/fiap/banco/App.java
+++ Banco/src/br/com/fiap/banco/App.java
@@ -3,24 +3,39 @@ package br.com.fiap.banco;
 public class App {
 
 	public static void main(String[] args) {
-		Conta contaUsuarioA = new Conta("1211");
+		Conta contaUsuarioA = new ContaPoupanca("1211");
 		contaUsuarioA.toString();
 		
-		Conta contaUsuarioB = new Conta("00588")
+		Conta contaUsuarioB = new ContaPoupanca("0588")
 ;		contaUsuarioB.toString();
+				
 		
-		ContaPoupanca poupanca = new ContaPoupanca("5652");
-		poupanca.sacar(10);
+		try {
+			contaUsuarioA.sacar(10);
+		} catch (SaldoInsuficienteException e) {
+			System.err.println("Um erro aconteceu: " + e.getMessage());
+		}
 		
 		
 		try {
-			contaUsuarioA.depositar(-2);
+			contaUsuarioB.depositar(100);
 		} catch (DepositoNegativoException e) {
 			System.err.println("Um erro aconteceu: " + e.getMessage());
 		}
 		
 		
+		System.out.println("Saldo usuário B = " + contaUsuarioB.getSaldo());
+		System.out.println("Saldo usuário A = " + contaUsuarioA.getSaldo());
 		
+		
+		try {
+			contaUsuarioB.transferir(150, contaUsuarioA);
+		} catch (SaldoInsuficienteException e) {
+			System.err.println("Um erro aconteceu: " + e.getMessage());
+		}
+		
+		System.out.println("Saldo usuário B = " + contaUsuarioB.getSaldo());
+		System.out.println("Saldo usuário A = " + contaUsuarioA.getSaldo());
 		
 		
 		
--- Banco/src/br/com/fiap/banco/Conta.java
+++ Banco/src/br/com/fiap/banco/Conta.java
@@ -1,6 +1,6 @@
 package br.com.fiap.banco;
 
-public class Conta {
+public abstract class Conta {
 	
 	private String numero;
 	private double saldo;
@@ -11,16 +11,27 @@ public class Conta {
 	
 	public void depositar(double valor) throws DepositoNegativoException {
 		if (valor < 0) {
-			throw new DepositoNegativoException("Não é possível fazer depósito de um valor negativo!");
+			throw new DepositoNegativoException("Não é possível fazer depósito de um valor negativo.");
 		}
 		saldo+=valor;
 	}
 	
-	public void sacar (double valor) {
-	
+	public void sacar (double valor) throws SaldoInsuficienteException {
+		if (valor > saldo) {
+			throw new SaldoInsuficienteException("Seu saldo não é suficiente para realizar esta operação.");
+		}		
 		saldo-= valor;
 	}
-
+	
+	
+	public void transferir (double valor, Conta contaDestinataria) throws SaldoInsuficienteException {
+		if (valor > saldo) {
+			throw new SaldoInsuficienteException("Seu saldo não é suficiente para realizar esta operação.");
+		}
+		this.saldo -= valor;
+		contaDestinataria.saldo += valor;
+	}
+	
 	public String getNumero() {
 		return numero;
 	}
